cf64d2a6177752e9bdd296fc8044484269e7eba3
Sai Abhinav Reddy Badinehal <113808535+abhinavbadin@users.noreply.github.com>
2025-10-30 15:12:58 -0400

    Time: 389 ms (92.19%) | Memory: 18 MB (92.40%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,38 +1,33 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # def func(ind1, ind2, t1, t2, dp):
-        #     if ind1 < 0 or ind2 < 0:
-        #         return 0
-            
-        #     if dp[ind1][ind2] != -1:
-        #         return dp[ind1][ind2]
-            
-        #     if t1[ind1] == t2[ind2]:
-        #         dp[ind1][ind2] =  1 + func(ind1 - 1, ind2 - 1, t1, t2, dp)
-        #     else:
-        #         dp[ind1][ind2] = max(func(ind1 - 1 , ind2, t1, t2, dp), func(ind1, ind2 - 1, t1, t2, dp))
-            
-        #     return dp[ind1][ind2]
-
-        n = len(text1)
-        m = len(text2)
-
-        #dp = [[-1 for j in range(m+1)] for i in range(n+1)]
-
-        prev = [0] * (m+1)
-        # for i in range(n+1):
-        #     dp[i][0] = 0
         
-        for j in range(m+1):
-            prev[j] = 0
+        def compare(i1,i2,dp):
+            if i1 < 0 or i2 < 0:
+                return 0
+            
+            if dp[i1][i2] != -1:
+                return dp[i1][i2]
+            
+            if text1[i1] == text2[i2]:
+                return 1 + compare(i1-1,i2-1,dp)
+            
+            dp[i1][i2] =  0 + max(compare(i1-1,i2,dp), compare(i1,i2-1,dp))
+            return dp[i1][i2]
         
-        for i in range(1,n+1):
-            curr = [0] * (m+1)
-            for j in range(1,m+1):
+        m = len(text1)
+        n = len(text2)
+        #dp = [[0 for j in range(n+1)] for i in range(m+1)]
+
+        prev = [0]*(n+1)
+
+        for i in range(1,m+1):
+            curr = [0]*(n+1)
+            for j in range(1,n+1):
                 if text1[i-1] == text2[j-1]:
                     curr[j] = 1 + prev[j-1]
                 else:
                     curr[j] = max(prev[j], curr[j-1])
             prev = curr
-        return prev[j]
-        # return func(n-1,m-1,text1, text2, dp)                
+        
+        return prev[n]
+        #return compare(m-1 , n-1, dp) 
