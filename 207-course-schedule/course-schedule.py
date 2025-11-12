a8ba51dc64fb48f0e0319eabec5273bf26bb18a8
Sai Abhinav Reddy Badinehal <113808535+abhinavbadin@users.noreply.github.com>
2025-11-11 20:59:36 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (92.41%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        #1. Build a graph
+        for dest,source in prerequisites:
+            graph[source].append(dest)
+            indegree[dest]+=1 
+        
+        #2 declare a dequeu and append ones with index 0
+        q = deque([i for i in range(numCourses) if indegree[i]==0])
+        count = 0
+
+        while q:
+            course = q.popleft()
+            count+=1
+
+            for nei in graph[course]:
+                indegree[nei]-=1
+
+                if indegree[nei]==0:
+                    q.append(nei)
+        
+        return count == numCourses 
