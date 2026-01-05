f97cd03e663be80eb6100337fa9cf95f17e931f8
Sai Abhinav Reddy Badinehal <113808535+abhinavbadin@users.noreply.github.com>
2026-01-04 23:32:27 -0500

    Time: 4 ms (59.39%) | Memory: 18.6 MB (86.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,10 +1,21 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        l,r = 0,1
-        while r<len(nums):
-            if nums[l] == 0 and nums[r]!=0:
-                nums[l],nums[r] = nums[r],nums[l]
-                l+=1
-            elif nums[l]!=0:
-                l+=1
-            r+=1
+        # l,r = 0,1
+        # while r<len(nums):
+        #     if nums[l] == 0 and nums[r]!=0:
+        #         nums[l],nums[r] = nums[r],nums[l]
+        #         l+=1
+        #     elif nums[l]!=0:
+        #         l+=1
+        #     r+=1
+
+        #Follow-up for minimizing the operations
+        #Try to traverse array get the non-zero elements forward
+        insert = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[insert] = nums[i]
+                insert+=1
+        
+        for i in range(insert,len(nums)):
+            nums[i] = 0 
